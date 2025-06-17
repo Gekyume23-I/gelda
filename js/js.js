799c22190f130fcb9879f801779f8320a34ede19
Gekyume <gekyume3d2y@gmail.com>
2025-06-17 14:08:42 +0200

    irene

--- js/js.js
+++ js/js.js
@@ -3,17 +3,20 @@ let currentIndex = 0;
 const items = document.querySelectorAll('.carousel-item');
 const totalItems = items.length;
 
+// Función para mostrar la siguiente imagen
 function showNextImage() {
     currentIndex = (currentIndex + 1) % totalItems;
     document.querySelector('.carousel-images').style.transform = `translateX(-${currentIndex * 100}%)`;
 }
 
+// Configurar el intervalo para cambiar la imagen cada 3 segundos
 setInterval(showNextImage, 3000); // Cambiar imagen cada 3 segundos
 
 // Mostrar frases con animación
 let fraseIndex = 0;
 const frases = document.querySelectorAll('.frases p');
 
+// Función para mostrar frases
 function showFrase() {
     frases[fraseIndex].style.display = 'block';
     frases[fraseIndex].style.animation = 'showFrases 5s ease-in-out';
@@ -24,4 +27,5 @@ function showFrase() {
     }, 5000); // Mostrar frase por 5 segundos
 }
 
-setInterval(showFrase, 7000); // Cambiar de frase cada 7 segundos
+// Cambiar de frase cada 7 segundos
+setInterval(showFrase, 7000);
